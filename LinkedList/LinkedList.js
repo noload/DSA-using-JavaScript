d9c065ff2461f2e4a868559745871071d1a5ade0
noload <vaibhavkamble023@gmail.com>
2024-01-13 12:03:29 +0530

    added pop method

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -10,7 +10,7 @@ class LinkedList {
     const newNode = new Node(value);
     this.head = newNode;
     this.tail = newNode;
-    this.lenght = 1;
+    this.length = 1;
   }
 
   push(value) {
@@ -20,7 +20,30 @@ class LinkedList {
       this.tail = newNode;
     } else {
       this.tail.next = newNode;
+      this.tail = newNode;
     }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
   }
 }
 
