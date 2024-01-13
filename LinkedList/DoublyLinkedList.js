58b9e7c2c2da114bad9f48b6f95ef4fd4752d353
noload <vaibhavkamble023@gmail.com>
2024-01-13 20:40:56 +0530

    added doubly linked list all methods

--- LinkedList/DoublyLinkedList.js
+++ LinkedList/DoublyLinkedList.js
@@ -27,4 +27,115 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    if (this.head == this.tail) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      temp = this.tail;
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    if (this.length === 1) {
+      this.head = this.tail = null;
+    } else {
+      temp = this.head;
+      this.head = this.head.next;
+      temp.next = null;
+      this.head.prev = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) {
+      return undefined;
+    }
+    if (index === 0) {
+      return this.head;
+    } else if (index === this.tail) {
+      return this.tail;
+    } else {
+      temp = this.head;
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+      return temp;
+    }
+  }
+
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    let newNode = new Node(value);
+    if (index > this.length || index < 0) {
+      return false;
+    }
+    if (index === 0) {
+      return this.unshift(value);
+    }
+    if (index === this.length) {
+      return this.push(value);
+    }
+    temp = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      temp = temp.next;
+    }
+    newNode.next = temp.next;
+    newNode.prev = temp;
+    temp.next.prev = newNode;
+    temp.next = newNode;
+    this.length++;
+    return this;
+  }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    const temp = this.get(index);
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+    temp.next = null;
+    temp.prev = null;
+    this.length--;
+    return temp;
+  }
 }
