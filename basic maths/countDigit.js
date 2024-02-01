dcbd2c4d18ef7c4a9aabc82909b557d5126380ab
noload <vaibhavkamble023@gmail.com>
2024-02-01 22:50:17 +0530

    count the  number of digit

---
+++ basic maths/countDigit.js
@@ -0,0 +1,16 @@
+// function countDigit(n) {
+//   let digit = 0;
+//   while (n >= 0) {
+//     digit++;
+//     n = n / 10;
+//   }
+//   return digit;
+// }
+
+// let n = 5000;
+
+// const digit = countDigit(n);
+// console.log(digit);
+
+count = Math.floor(Math.log10(4000) + 1);
+console.log(count);
